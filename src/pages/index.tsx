70eb7b0baae5ab0458a105341e46a119447196f1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-16 20:31:32 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,49 @@
 import type { NextPage } from 'next'
+import Image from 'next/future/image'
+import Link from 'next/link'
+
+import tShirt1 from '../assets/t-shirts/1.png'
+import tShirt2 from '../assets/t-shirts/2.png'
+// import tShirt3 from '../assets/t-shirts/3.png'
+import * as S from '../styles/pages/home'
 
 const Home: NextPage = () => {
-  return <h1>Home</h1>
+  return (
+    <S.Container>
+      <Link href="/product/1" passHref>
+        <S.Product>
+          <Image src={tShirt1} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </S.Product>
+      </Link>
+
+      <Link href="/product/2" passHref>
+        <S.Product>
+          <Image src={tShirt2} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </S.Product>
+      </Link>
+
+      {/* <Link href="/product/3" passHref>
+        <S.Product>
+          <Image src={tShirt3} width={520} height={480} alt="" />
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </S.Product>
+      </Link> */}
+    </S.Container>
+  )
 }
 
 export default Home
---
+++ src/styles/pages/home.ts
@@ -0,0 +1,57 @@
+import { styled } from '..'
+
+export const Container = styled('main', {
+  display: 'flex',
+  gap: '3rem',
+  width: '100%',
+  maxWidth: 'calc(100vw - ((100vw - 1180px) / 2))',
+  marginLeft: 'auto',
+  minHeight: 656,
+})
+
+export const Product = styled('a', {
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100% )',
+  borderRadius: 8,
+  padding: '0.25rem',
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  overflow: 'hidden',
+
+  img: {
+    objectFit: 'cover',
+  },
+
+  footer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    position: 'absolute',
+    bottom: '0.25rem',
+    left: '0.25rem',
+    right: '0.25rem',
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    padding: '2rem',
+    borderRadius: 6,
+    transform: 'translateY(110%)',
+    opacity: 0,
+    transition: 'all 200ms ease-in-out',
+  },
+
+  'footer > strong': {
+    fontSize: '$lg',
+  },
+
+  'footer > span': {
+    fontSize: '$xl',
+    fontWeight: 'bold',
+    color: '$green300',
+  },
+
+  '&:hover > footer': {
+    transform: 'translateY(0%)',
+    opacity: 1,
+  },
+})
