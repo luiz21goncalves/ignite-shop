21ae83a5437be0f1eda18e49cb86a82614385cb7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-19 19:03:12 -0300

    feat: add products carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
+import { useKeenSlider } from 'keen-slider/react'
 import type { NextPage } from 'next'
 import Image from 'next/future/image'
 import Link from 'next/link'
 
 import tShirt1 from '../assets/t-shirts/1.png'
 import tShirt2 from '../assets/t-shirts/2.png'
-// import tShirt3 from '../assets/t-shirts/3.png'
+import tShirt3 from '../assets/t-shirts/3.png'
 import * as S from '../styles/pages/home'
 
+import 'keen-slider/keen-slider.min.css'
+
 const Home: NextPage = () => {
+  const [sliderRef] = useKeenSlider({
+    slides: { perView: 3, spacing: 48 },
+  })
+
   return (
-    <S.Container>
+    <S.Container ref={sliderRef} className="keen-slider">
       <Link href="/product/1" passHref>
-        <S.Product>
+        <S.Product className="keen-slider__slide">
           <Image src={tShirt1} width={520} height={480} alt="" />
 
           <footer>
@@ -22,7 +29,7 @@ const Home: NextPage = () => {
       </Link>
 
       <Link href="/product/2" passHref>
-        <S.Product>
+        <S.Product className="keen-slider__slide">
           <Image src={tShirt2} width={520} height={480} alt="" />
 
           <footer>
@@ -32,8 +39,8 @@ const Home: NextPage = () => {
         </S.Product>
       </Link>
 
-      {/* <Link href="/product/3" passHref>
-        <S.Product>
+      <Link href="/product/3" passHref>
+        <S.Product className="keen-slider__slide">
           <Image src={tShirt3} width={520} height={480} alt="" />
 
           <footer>
@@ -41,7 +48,7 @@ const Home: NextPage = () => {
             <span>R$ 79,90</span>
           </footer>
         </S.Product>
-      </Link> */}
+      </Link>
     </S.Container>
   )
 }
