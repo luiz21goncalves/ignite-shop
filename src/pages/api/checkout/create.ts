87a44afe97bd81376110458a7a122c4928470211
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-22 18:30:26 -0300

    chore: show purchased product info

--- src/pages/api/checkout/create.ts
+++ src/pages/api/checkout/create.ts
@@ -15,7 +15,7 @@ export default async function handle(
       })
     }
 
-    const successUrl = `${process.env.NEXT_URL}/success`
+    const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
     const cancelUrl = `${process.env.NEXT_URL}/`
 
     const sessionCheckout = await stripe.checkout.sessions.create({
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,22 +1,31 @@
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import Image from 'next/future/image'
 import Link from 'next/link'
+import { Stripe } from 'stripe'
 
+import { stripe } from '../lib/stripe'
 import * as S from '../styles/pages/success'
 
-const Success: NextPage = () => {
+type SuccessProps = {
+  customerName: string
+  product: {
+    name: string
+    imageUrl: string
+  }
+}
+
+const Success: NextPage<SuccessProps> = ({ customerName, product }) => {
   return (
     <S.Container>
       <h1>Compra efetuada!</h1>
 
       <S.ImageContainer>
-        <Image />
+        <Image alt="" src={product.imageUrl} width={120} height={110} />
       </S.ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{customerName}</strong>, sua{' '}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">
@@ -24,6 +33,29 @@ const Success: NextPage = () => {
       </Link>
     </S.Container>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = query.session_id as string
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product'],
+  })
+
+  const product = session.line_items?.data[0].price?.product as Stripe.Product
+  const customerName = session.customer_details?.name
+
+  console.log({ customer: session.customer_details })
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  }
 }
 
 export default Success
