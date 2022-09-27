0d7308c87c24360ed69d28102921d1785e646b5f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2022-09-27 16:47:29 -0300

    feat: add cart link in header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/future/image'
+import Link from 'next/link'
+import { Handbag } from 'phosphor-react'
 
 import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
@@ -11,7 +13,19 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <S.Container>
       <S.Header>
-        <Image src={logoImg} alt="" />
+        <nav>
+          <Link href="/" prefetch={false} passHref>
+            <a>
+              <Image src={logoImg} alt="" />
+            </a>
+          </Link>
+
+          <Link href="/cart" prefetch={false} passHref>
+            <S.CartButton>
+              <Handbag weight="bold" size={24} />
+            </S.CartButton>
+          </Link>
+        </nav>
       </S.Header>
       <Component {...pageProps} />
     </S.Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -11,6 +11,48 @@ export const Container = styled('div', {
 export const Header = styled('header', {
   padding: '2rem 0',
   width: '100%',
-  maxWidth: 1180,
+  maxWidth: 1168,
   margin: '0 auto',
+
+  nav: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+})
+
+export const CartButton = styled('a', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  padding: '0.75rem',
+  color: '$gray500',
+  backgroundColor: '$gray800',
+  borderRadius: 8,
+  transition: 'all 200ms',
+  position: 'relative',
+
+  '&:hover': {
+    opacity: 0.7,
+  },
+
+  '&::before': {
+    content: '1',
+    position: 'absolute',
+    width: '1.5rem',
+    height: '1.5rem',
+    lineHeight: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: '50%',
+    top: '-0.75rem',
+    right: '-0.75rem',
+    backgroundColor: '$green300',
+    fontSize: '$sm',
+    color: '$white',
+    fontWeight: 'bold',
+    outline: '3px solid $gray900',
+  },
 })
